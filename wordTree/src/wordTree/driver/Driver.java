a5bffd445876c10b90d9ab1a6489105cedb3bbf4
poorvitusam <poorvitusam@gmail.com>
2017-11-01 01:48:04 -0400

    Reading command line arguments

--- wordTree/src/wordTree/driver/Driver.java
+++ wordTree/src/wordTree/driver/Driver.java
@@ -1,5 +1,11 @@
 package src.wordTree.driver;
 
 public class Driver {
-
+	public static void main(String args[]) {
+		String inputFile = args[0];
+		String outputFile = args[1];
+		int NUM_THREADS = Integer.parseInt(args[2]);
+		String words = args[3];
+		int debugLevel = Integer.parseInt(args[4]);
+	}
 }
